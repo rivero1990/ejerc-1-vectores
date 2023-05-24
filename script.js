b4ba5d109e82e57bb4536abf2f569c430949b197
Anibal Rivero <riveroleonel@gmail.com>
2023-05-24 17:09:25 -0300

    ejercicio inicial 1 vectores

---
+++ script.js
@@ -0,0 +1,7 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7,];
+
+console.log( numeros[0]);
+console.log( numeros[3]);
+console.log( numeros[numeros.length-1]);
+
+console.log(numeros[5] + " El vector se llama 'numeros' y es de tipo de dato 'number'");
